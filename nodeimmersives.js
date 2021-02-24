311b485298a3de7fc373dd03857cb07970b8a092
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-02-23 17:23:30 -0700

    uppercase

--- nodeimmersives.js
+++ nodeimmersives.js
@@ -1,8 +1,9 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
 const getInput = require("./get-input.js");
 
-const user = getInput(1);
-const name = getInput(2);
+const firstName = getInput(1);
+const lastName = getInput(2);
 
 console.log("Hello, " + getInput(1) + " " + getInput(2))
+console.log(firstName.toUpperCase() + " " + lastName.toUpperCase())
 
